be870ede96d766c03da36e5fa69ffc27158b2308
muxthafa <musthafamohd0@gmail.com>
2021-08-18 21:08:56 +0530

    Single player start position zero

--- LadderSnake.java
+++ LadderSnake.java
@@ -1,8 +1,13 @@
 package com.bridgelabs;
 
 public class LadderSnake {
+	public static final int no_play = 1;
+	public static final int Ladder = 2;
+	public static final int Snake = 3;
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake ladder problem");
+		int pos = 0;
+		int count=0;
 	}
 }
 
